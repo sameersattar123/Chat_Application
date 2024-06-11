ede77ebccda1bbb0f6eec1c75b89871949baf8f5
Sameer <sameersattar1111@gmail.com>
2024-06-11 19:30:45 +0500

    complete design

--- src/components/List/ChatList/ChatList.jsx
+++ src/components/List/ChatList/ChatList.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./ChatList.css";
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
   return (
-    <div>ChatList</div>
-  )
-}
+    <div className="ChatList">
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search (1).png" alt="" />
+          <input type="text" placeholder="Search" />
+        </div>
+        <img
+          className="add"
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          onClick={() => setAddMode(!addMode)}
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Sameer</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Sameer</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Sameer</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ChatList
+export default ChatList;
