123ec6bf302aae6d49b1470ea96ccf72014acb78
Sameer <sameersattar1111@gmail.com>
2024-06-14 03:37:31 +0500

    complete

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,10 +1,87 @@
-import React from 'react'
-import "./Detail.css"
+import React from "react";
+import "./Detail.css";
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
-  )
-}
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Sameer Sattar</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy % help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+              <div className="photos">
+          <div className="photoItem">
+            <div className="photoDetail">
+              <img
+                src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                alt=""
+              />
+              <span>photo_2024_2.png</span>
+            </div>
+            <img src="./download (5).png" alt="" className="icon" />
+          </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download (5).png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download (5).png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download (5).png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
 
-export default Detail
+        <button>Block User</button>
+        <button className="logout">Logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default Detail;
